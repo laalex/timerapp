47ea176544135644278bbbf1c6ae6dfd7412fe20
Alexandru Lamba <roshkattu94@gmail.com>
2015-03-19 18:13:41 +0200

    Working on the FE modules

--- app/assets/javascripts/modules/task.module.js
+++ app/assets/javascripts/modules/task.module.js
@@ -6,14 +6,63 @@
  */
 var TaskModule = (function (module) {
 
+  var CONSTANTS = {
+    _currentId: null,
+    _isCounting: false,
+    _startDuration: null,
+    _currentDuration: null,
+    _currentInterval: null
+  }
+
   // Private members
   var _init = function(){
     console.log("TaskModule inited;");
   }
 
-  // Public members
+  // Set current ID
+  var _setCurrent = function(id){
+    CONSTANTS._currentId = id;
+    return id;
+  }
+  // Start counting
+  var _startCounting = function(start){
+    // Set the start duration
+    CONSTANTS._startDuration = start;
+    // Update the state to counting
+    CONSTANTS._isCounting = true;
+    // Start the countdown
+    _startCount();
+    // Update the UI with the current duration
+    UI.updateTaskCount(CONSTANTS._currentId, CONSTANTS._currentDuration);
+  }
+
+  // Stop counting
+  var _stopCounting = function(){
+    // Set the counting state to false
+    CONSTANTS._isCounting = true;
+  }
+
+  // Start counting
+  var _startCount = function(){
+    // Create an interval for the current item and do the countdown
+  }
+
+
+  // Public members -> Export module functions
   module.init = function(){
     _init();
+  }
+
+  module.setCurrent = function(id){
+    _setCurrent(id);
+  }
+
+  module.startCounting = function(start){
+    _startCounting(start);
+  }
+
+  module.stopCounting = function(){
+    _stopCounting();
   }
 
 
--- app/assets/javascripts/modules/ui.module.js
+++ app/assets/javascripts/modules/ui.module.js
@@ -8,13 +8,44 @@ var UI  = (function (module) {
   // Private members
   var _init = function(){
     console.log("UIModule inited;");
+    _attachEventListenrs();
   }
+
+  // Update the task count on an interface
+  var _updateTaskCountInterface = function(id, duration){
+    // Find the element with ID
+
+    // Update the duration
+  }
+
+  // Attach event listeners
+  var _attachEventListenrs = function(){
+
+  }
+
+  // Popups UI
+  var _popups = {
+    showPopup: function(wrapper, title, message, actions){
+      // Show a popup in a wrapper with a title, message and optional actions buttons
+    },
+
+    hidePopups: function(){
+      $(".popup" ).hide();
+    }
+  }
+
 
   // Public members
   module.init = function(){
     _init();
   }
 
+  module.updateTaskCount = function(id, duration){
+    _updateTaskCountInterface(id, duration);
+  }
+
+  // Popup UI object
+  module.popup = _popups;
 
   return module;
 })(UI || {});
